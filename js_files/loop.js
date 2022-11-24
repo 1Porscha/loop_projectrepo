acfc53e89e30540ae5263d6d296b87699d531877
1Porscha <117795389+1Porscha@users.noreply.github.com>
2022-11-24 09:06:16 -0500

    Loop-project

---
+++ js_files/loop.js
@@ -0,0 +1,32 @@
+console.log("working great")
+
+//Write a for loop that logs 0-15
+
+for (var i = 0; i <= 15; i ++){
+    console.log(i)
+}
+
+//Write a for loop that counts from 15-0
+
+for (var i = 15; i >= 0; i --){
+    console.log(i)
+}
+
+//Make variable laps = 0; write a for loop that logs (“that’s another lap!”) and adds 1 to the laps variable. AFTER the loop, log laps.
+
+let laps = 0
+for ( i = 0; i <= 0; i++ ) {
+    console.log("that's another lap!")
+}
+
+// Make a loop from 0-100. In every loop check if i == age, if so log something, else say “age doesn’t match”.
+
+let age=32
+for (var i = 0; i <= 100; i++){
+if (i == age)
+console.log ("age match")
+
+else {
+    console.log("age doesn't match")
+}
+}
